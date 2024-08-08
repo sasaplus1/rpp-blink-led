d1be5f672d0482664434720415cc3a1adee11d88
sasaplus1 <sasaplus1@gmail.com>
2024-08-09 00:24:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# rpp-blink-led
+
+Blinking LED on the Raspberry Pi Pico
+
+## License
+
+The MIT license
---
+++ index.ts
@@ -0,0 +1,9 @@
+import { delay } from "@devicescript/core"
+import { setStatusLight } from "@devicescript/runtime"
+
+setInterval(async () => {
+  await setStatusLight(0x00ff00) // green
+  await delay(500)
+  await setStatusLight(0x000000) // off
+  await delay(500)
+}, 500)
