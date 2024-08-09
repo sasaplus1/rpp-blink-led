7191a843b8c086d8c999117cd95783b03de42f43
sasaplus1 <sasaplus1@gmail.com>
2024-08-09 21:39:16 +0900

    improve

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
-import { delay } from "@devicescript/core";
-import { setStatusLight } from "@devicescript/runtime";
+import { pins } from '@dsboard/pico';
+import { startLightBulb } from "@devicescript/servers";
+import { delay, gpio, GPIOMode } from "@devicescript/core"
+import { pinMode } from '@devicescript/gpio';
+import { setStatusLight } from "@devicescript/runtime"
+
+const led2 = startLightBulb({
+  pin: pins.GP16,
+});
 
 setInterval(async () => {
-  await setStatusLight(0x00ff00); // green
-  await delay(500);
-  await setStatusLight(0x000000); // off
-  await delay(500);
-}, 500);
+  let b, n;
+
+  await setStatusLight(0x00ff00) // green
+  b = await led2.intensity.read();
+  n = b > 0 ? 0 : 1;
+  await led2.intensity.write(n);
+
+  await delay(500)
+
+  await setStatusLight(0x000000) // off
+  b = await led2.intensity.read();
+  n = b > 0 ? 0 : 1;
+  await led2.intensity.write(n);
+
+  await delay(500)
+}, 500)
